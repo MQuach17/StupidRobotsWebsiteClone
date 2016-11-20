3c7a2afe84224b71f57101afbdf0006d263c2f5c
toristyner <tori.styner@gmail.com>
2016-11-20 21:29:56 +0000

    media slider and styles

--- media/media.controller.js
+++ media/media.controller.js
@@ -11,6 +11,32 @@ function MediaController ($scope, $rootScope, $route, $routeParams, $location, $
     $scope.$routeParams = $routeParams;
 
     var vm = this;
+// photos
+    $scope.photos = [
+    	{src: "https://s3.amazonaws.com/stupid-robots/background_bolts.png",desc:"Image 1"},
+		{src: "https://s3.amazonaws.com/stupid-robots/stupid_robots_logo.png",desc:"Image 2"},
+    	{src: "https://s3.amazonaws.com/stupid-robots/top_nav_picture.png",desc:"Image 3"}
+    ];
+    // default to first photo
+    $scope._Index = 0;
+    // active index
+    $scope.isActive = function(index){
+    	return $scope._Index === index;
+    }
 
+    // prev image
+    $scope.showPrev = function(){
+    	$scope._Index = ($scope._Index > 0) ? --$scope._Index : $.photos.length - 1;
+    };
+
+    // next image
+    $scope.showNext = function(){
+    	$scope._Index = ($scope._Index < $scope.photos.length -1) ? ++$scope._Index : 0;
+    };
+
+    // show clicked image
+    $scope.showPhoto = function(index){
+    	$scope._Index = index;
+    }
 }
 
