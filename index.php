4af60ef2a9196e71f82bd10827ca3eb6f79eb473
toristyner <tori.styner@gmail.com>
2016-11-20 17:32:42 +0000

    setting up

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
+<!DOCTYPE html>
 <html>
 
-Hey Quach
+	<head>
+
+		<title>The Stupid Robots</title>
+		
+		<!-- Angular -->
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+		
+		<!-- Angular Material -->
+		<script src="https://ajax.googleapis.com/ajax/libs/angular_material/1.1.1/angular-material.min.js"></script>
 
 
-</html>
+	</head>
+
+	<body>
+
+
+
+	</body>
+
+</html>	
 
---
+++ script.js
@@ -0,0 +1,51 @@
+(function(angular) {
+  'use strict';
+angular.module('SiteApp', ['ngRoute','ngMaterial'])
+.run(function($rootScope) {
+})
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'home/home.html',
+    controller: 'MainController',
+    controllerAs: 'vm'
+  })
+  .when('/Home', {
+    templateUrl: 'home/home.html',
+    controller: 'MainController',
+    controllerAs: 'vm'
+  })
+  .when('/Media', {
+    templateUrl: 'media/media.html',
+    controller: 'MediaController',
+    controllerAs: 'vm'
+  })
+  .when('/Events', {
+    templateUrl: 'events/events.html',
+	controller: 'EventsController',
+	controllerAs: 'vm'
+  })
+  .when('/ContactUs', {
+	templateUrl: 'contact/contact.html',
+	controllerAs: 'vm'
+  })
+  .when('/About', {
+	templateUrl: 'about/about.html',
+	controllerAs: 'vm'
+  })
+.otherwise({
+    redirectTo: '/home.html'
+  });
+})
+.run(function($rootScope, $location, $window) {
+
+    $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+
+    });
+
+    angular.element(window).on("resize", function() {
+        $rootScope.$apply();
+    });
+
+});
+})(window.angular);
